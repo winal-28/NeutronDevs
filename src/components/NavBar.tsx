7985ea4ecfce8e14254d33d0b30e18396d24d04d
Alwin Villero <winalrollevi@gmail.com>
2021-04-06 16:38:29 +0800

    Made the NavBar component, also responsive

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <h4 className="logo">NeutronDevs</h4>
+            <a href="#" className='joinServer'>Discord</a>
+        </nav>
+    )
+}
